e61a8a76320f7666801a9740614f00506f4e9cfa
neil.fraser@gmail.com <neil.fraser@gmail.com@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2013-05-27 23:16:20 +0000

    Add repeat block that use number block instead of inline input field.  Factor out number detection in generators.  Fix fractional slices in Python. Review URL: https://codereview.appspot.com/9784043
    git-svn-id: http://blockly.googlecode.com/svn/branches/mooc@1015 c400ca83-b69d-9dd7-9705-49c6b8615e23

---
+++ static/common.js
@@ -0,0 +1,155 @@
+/**
+ * Visual Blocks Editor
+ *
+ * Copyright 2013 Google Inc.
+ * http://blockly.googlecode.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Common support code for Blockly apps.
+ * @author fraser@google.com (Neil Fraser)
+ */
+
+'use strict';
+
+/**
+ * The richness of block colours, regardless of the hue.
+ * MOOC blocks should be brighter (target audience is younger).
+ * Must be in the range of 0 (inclusive) to 1 (exclusive).
+ * Blockly's default is 0.45.
+ */
+Blockly.HSV_SATURATION = 0.6;
+
+Blockly.Apps = {};
+
+/**
+ * Load the specified language file(s).
+ * @param {!Array<string>} languageSrc Array of language files.
+ */
+Blockly.Apps.loadLanguageScripts = function(languageSrc) {
+  for (var x = 0; x < languageSrc.length; x++) {
+    var file = languageSrc[x];
+    if (file.match(/^(\w+\/)*\w+\.js$/)) {
+      document.writeln('<script type="text/javascript" ' +
+          'src="../' + file + '"><' + '/script>');
+    } else {
+      console.error('Illegal language file: ' + file);
+    }
+  }
+};
+
+/**
+ * Updates the document's 'capacity' element's innerHTML with a message
+ * indicating how many more blocks are permitted.  The capacity
+ * is retrieved from Blockly.mainWorkspace.remainingCapacity().
+ * @param {!Object} MSG An object with appropriate text properties for
+ *     capacity0, capacity1, and capacity2.
+ */
+Blockly.Apps.updateCapacity = function(MSG) {
+  var cap = Blockly.mainWorkspace.remainingCapacity();
+  var p = document.getElementById('capacity');
+  if (cap == Infinity) {
+    p.style.display = 'none';
+  } else {
+    p.style.display = 'inline';
+    if (cap == 0) {
+      p.innerHTML = MSG.capacity0;
+    } else if (cap == 1) {
+      p.innerHTML = MSG.capacity1;
+    } else {
+      cap = Number(cap);
+      p.innerHTML = MSG.capacity2.replace('%1', cap);
+    }
+  }
+};
+
+/**
+ * Congratulates the user for completing the level and offers to
+ * direct them to the next level, if available.
+ * @param {?number} page The current page.
+ * @param {number} level The current level.
+ * @param {number} maxLevel The maxmium available level.
+ * @param {!Object} MSG An object with appropriate text properties for
+ *     MSG.nextLevel and MSG.finalLevel.
+ */
+Blockly.Apps.congratulations = function(page, level, maxLevel, MSG) {
+  if (level < maxLevel) {
+    var proceed = window.confirm(MSG.nextLevel.replace('%1', level + 1));
+    if (proceed) {
+      var url = window.location.protocol + '//' +
+          window.location.host + window.location.pathname + '?';
+      if (page) {
+        url += 'page=' + page + '&';
+      }
+      url += 'level=' + (level + 1);
+      window.location = url;
+    }
+  } else {
+    window.alert(MSG.finalLevel);
+  }
+};
+
+/**
+ * Highlight the block (or clear highlighting).
+ * @param {?string} id ID of block that triggered this action.
+ */
+Blockly.Apps.highlight = function(id) {
+  if (id) {
+    var m = id.match(/^block_id_(\d+)$/)
+    if (m) {
+      id = m[1];
+    }
+  }
+  Blockly.mainWorkspace.highlightBlock(id);
+};
+
+/**
+ * If the user has executed too many actions, we're probably in an infinite
+ * loop.  Sadly I wasn't able to solve the Halting Problem.
+ * @param {?string} opt_id ID of loop block to highlight if timeout is reached.
+ * @throws {false} Throws an error to terminate the user's program.
+ */
+Blockly.Apps.checkTimeout = function(opt_id) {
+  if (opt_id) {
+    Blockly.Apps.log.push([null, opt_id]);
+  }
+  if (Blockly.Apps.ticks-- < 0) {
+    // Highlight an infinite loop on death.
+    throw false;
+  }
+};
+
+/**
+ * Convert the user's code to raw JavaScript.
+ * @param {string} code Generated code.
+ * @return {string} The code without serial numbers and timeout checks.
+ */
+Blockly.Apps.stripCode = function(code) {
+  // Strip out serial numbers.
+  code = code.replace(/(,\s*)?'block_id_\d+'\)/g, ')');
+  // Remove timeouts.
+  var regex = new RegExp(Blockly.JavaScript.INFINITE_LOOP_TRAP
+      .replace('(%1)', '\\(\\)'), 'g');
+  return code.replace(regex, '');
+};
+
+/**
+ * Show the user's code in raw JavaScript.
+ */
+Blockly.Apps.showCode = function() {
+  var code = Blockly.Generator.workspaceToCode('JavaScript');
+  code = Blockly.Apps.stripCode(code);
+  window.alert(code);
+};
---
+++ static/turtle/answers.js
@@ -0,0 +1,149 @@
+/**
+ * Blockly Demo: Turtle Graphics
+ *
+ * Copyright 2013 Google Inc.
+ * http://blockly.googlecode.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Sample answers for Turtle levels. Used for prompts and marking.
+ * @author fraser@google.com (Neil Fraser)
+ */
+'use strict';
+
+/**
+ * Sample solutions for each level.
+ * To create an answer, just solve the level in Blockly, then paste the
+ * resulting JavaScript here, moving any functions to the beginning of
+ * this function.
+ */
+Turtle.answer = function() {
+  function colour_random() {
+    var num = Math.floor(Math.random() * Math.pow(2, 24));
+    return '#' + ('00000' + num.toString(16)).substr(-6);
+  }
+  if (Turtle.PAGE == 1) {
+    switch (Turtle.LEVEL) {
+      case 1:
+        // Line.
+        Turtle.moveForward(100);
+        break;
+     case 2:
+        // Square.
+        Turtle.moveForward(100);
+        Turtle.turnRight(90);
+        Turtle.moveForward(100);
+        Turtle.turnRight(90);
+        Turtle.moveForward(100);
+        Turtle.turnRight(90);
+        Turtle.moveForward(100);
+        break;
+      case 3:
+        // Use repeat to draw a square.
+        for (var count = 0; count < 4; count++) {
+          Turtle.moveForward(100);
+          Turtle.turnRight(90);
+        }
+        break;
+      case 4:
+        // Red square.
+        Turtle.penColour('ff0000');  // red
+        for (var count = 0; count < 4; count++) {
+          Turtle.moveForward(100);
+          Turtle.turnRight(90);
+        }
+        break;
+      case 5:
+        // Blue square.
+        // TODO: Allow a range of shades of blue.
+        Turtle.penColour('0000ff');  // blue
+        for (var count = 0; count < 4; count++) {
+          Turtle.moveForward(100);
+          Turtle.turnRight(90);
+        }
+        break;
+      case 6:
+        // Equilateral triangle.
+        for (var count = 0; count < 3; count++) {
+          Turtle.moveForward(100);
+          Turtle.turnRight(120);
+        }
+        break;
+      case 7:
+        // Arrow.
+        for (var count = 0; count < 3; count++) {
+          Turtle.moveForward(100);
+          Turtle.turnRight(120);
+        }
+        Turtle.moveForward(50);
+        Turtle.turnLeft(90);
+        Turtle.moveForward(100);
+        break;
+      case 8:
+        // House.
+        for (var count = 0; count < 4; count++) {
+          Turtle.moveForward(75);
+          Turtle.turnRight(90);
+        }
+        Turtle.moveForward(75);
+        Turtle.turnRight(30);
+        Turtle.moveForward(75);
+        Turtle.turnRight(120);
+        Turtle.moveForward(75);
+        break;
+      case 9:
+        // Pinwheel.
+        for (var count2 = 0; count2 < 36; count2++) {
+          for (var count = 0; count < 4; count++) {
+            Turtle.moveForward(100);
+            Turtle.turnRight(90);
+          }
+          Turtle.turnRight(10);
+        }
+        break;
+      case 10:
+        // Colourful pinwheel.
+        for (var count2 = 0; count2 < 36; count2++) {
+          Turtle.penColour(colour_random());
+          for (var count = 0; count < 4; count++) {
+            Turtle.moveForward(100);
+            Turtle.turnRight(90);
+          }
+          Turtle.turnRight(10);
+        }
+        break;
+    }
+  } else if (Turtle.PAGE == 2) {
+    switch (Turtle.LEVEL) {
+      case 1:
+        // Square in fewest possible blocks.
+        for (var count = 0; count < 4; count++) {
+          Turtle.moveForward(100);
+          Turtle.turnRight(90);
+        }
+      break;
+    }
+  }
+};
+
+/**
+ * Validate whether the user's answer is correct.
+ * @param {number} pixelErrors Number of pixels that are wrong.
+ * @return {boolean} True if the level is solved, false otherwise.
+ */
+Turtle.isCorrect = function(pixelErrors) {
+  console.log('Pixel errors: ' + pixelErrors);
+  return Turtle.LEVEL < Turtle.MAX_LEVEL && pixelErrors < 100;
+};
