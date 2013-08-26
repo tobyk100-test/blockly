b43d6342ac049e5e87a3a7c2cf7374afcf1cba41
Nan <nanli@Nans-MacBook-Pro-2.local>
2013-08-26 13:09:20 -0700

    Added the additional level in Karel 2 to motivate functions.

--- branches/mooc/static/karel/maze.js
+++ branches/mooc/static/karel/maze.js
@@ -40,7 +40,7 @@ document.write('<script type="text/javascript" src="generated/' +
 
 Maze.MAX_REINF = 0;
 Maze.PAGE = BlocklyApps.getNumberParamFromUrl('page', 1, 2);
-Maze.MAX_LEVEL = [undefined, 10, 8][Maze.PAGE];
+Maze.MAX_LEVEL = [undefined, 10, 9][Maze.PAGE];
 Maze.LEVEL = BlocklyApps.getNumberParamFromUrl('level', 1, Maze.MAX_LEVEL);
 Maze.REINF = BlocklyApps.getNumberParamFromUrl('reinf', 1, Maze.MAX_REINF);
 
