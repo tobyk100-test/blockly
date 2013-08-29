1d9dca3287b121d388627d6834ad2ae804ea65df
Toby <tobyk100@gmail.com>
2013-08-29 16:54:53 -0700

    Added handler on link click which loads xml into workspace (broken). Added unescaping of xml

--- branches/mooc/static/statistics/statistics.js
+++ branches/mooc/static/statistics/statistics.js
@@ -3,23 +3,40 @@ function attachProgramListeners() {
   for (link in links) {
     link.addEventListener(link, function() {
       var program = link.innerHTML;
-      alert("Program: " + program);
     });
   }
 }
-function init() {
+function linkHandler(e) {
+  var link = e.target;
+  var program = unescapeHtml(link.innerHTML);
+  var Blockly = document.getElementById('mazeFrame').contentWindow['Blockly'];
+  Blockly.mainWorkspace.clear();
+  Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, program);
+}
+// TODO(toby): Use a library function to ensure XSS proofness, see closure's
+// string.unescapeEntities
+function unescapeHtml(html) {
+  var unescapedHtml = html.replace(/&lt;|&gt;/g, function(entity) {
+    var map = {"&lt;": "<", "&gt;": ">"};
+    return map[entity];
+  });
+  return unescapedHtml;
+}
+function buildTable(programs) {
   var resultTable = document.querySelector('.results > table');
+  for (var program in programs) {
+    var row = Templates.programRow(programs[program][0], programs[program][1],
+        program, linkHandler);
+    resultTable.appendChild(row);
+  }
+  attachProgramListeners();
+}
+function init() {
   var req = new XMLHttpRequest();
   req.open("get", "/report", true);
   req.onload = function(e) {
     programs = JSON.parse(req.responseText);
-    for (var program in programs) {
-      var row = Templates.programRow(programs[program][0],
-                                     programs[program][1],
-                                     program);
-      resultTable.appendChild(row);
-    }
-    attachProgramListeners();
+    buildTable(programs);
   };
   req.send()
 }
--- branches/mooc/static/statistics/templates.js
+++ branches/mooc/static/statistics/templates.js
@@ -4,7 +4,7 @@ Templates.createTableCell = function(contents) {
   cell.appendChild(document.createTextNode(contents));
   return cell;
 }
-Templates.programRow = function(success, count, program) {
+Templates.programRow = function(success, count, program, handler) {
   var row = document.createElement('tr');
   var success_ = Templates.createTableCell(success);
   var count_ = Templates.createTableCell(count);
@@ -13,13 +13,14 @@ Templates.programRow = function(success, count, program) {
   row.appendChild(success_);
   row.appendChild(count_);
   row.appendChild(program_);
-  Templates.addLink(program_);
+  Templates.addLink(program_, handler);
   return row;
 }
-Templates.addLink = function(cell) {
+Templates.addLink = function(cell, handler) {
   var contents = cell.firstChild;
   var link = document.createElement('a');
   link.href = '#';
   link.appendChild(contents);
   cell.appendChild(link);
+  link.addEventListener('click', handler);
 }
